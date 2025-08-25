e3c87c52da3c35f7fb6a4487af1b2e23da0d999e
04aks <abdellahssehib@gmail.com>
2025-08-25 05:35:31 +0100

    THE great disposal of THE unwanted files

--- src/main/java/aks/internal/statics/Commands.java
+++
@@ -1,7 +0,0 @@
-package aks.internal.statics;
-
-public class Commands {
-    public static final String AUTH = "/auth";
-    public static final String AVA_CURR = "/ac";
-    public static final String SELECT_ITEM = "/si";
-}
